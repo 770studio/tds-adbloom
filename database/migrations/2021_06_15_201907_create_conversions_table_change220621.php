14fb8f09cb21ace93cd40add3cedae3281154071
770-2 desctop <iluxa@770studio.ru>
2021-06-22 17:19:30 +0300

    some fields casing string truncate pdo error

---
+++ database/migrations/2021_06_15_201907_create_conversions_table_change220621.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConversionsTableChange220621 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('conversions', function (Blueprint $table) {
+
+            $table->string("source" )->nullable()->change();
+            $table->text("refer" )->default('')->change();
+            $table->text("user_agent" )->default('')->change();
+
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversions');
+    }
+}
