8bd9afdf005640d65fc71a82238e426c7fa88110
770-2 desctop <iluxa@770studio.ru>
2021-07-08 12:26:43 +0300

    add RedirectStatuses Seeder

---
+++ database/seeds/RedirectStatusesSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RedirectStatusesSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        DB::table('redirect_statuses')->truncate();
+        DB::table('redirect_statuses')->insert([
+            ['code' => 'success'],
+            ['code' => 'reject'],
+            ['code' => 'oq'],
+            ['code' => 'dq']
+        ]);
+
+
+    }
+}
