778a85e05531936b2b8cf2b331c68f77f2037a60
Илья <vayner.ilya@ya.ru>
2021-07-14 02:08:49 +0300

    partner postback

--- app/Jobs/doPartnerPostBack.php
+++ app/Jobs/doPartnerPostBack.php
@@ -39,6 +39,24 @@ class doPartnerPostBack implements ShouldQueue
         if(!$this->conversion->Partner || !$this->conversion->Opportunity) return;
         if(!$this->conversion->Partner->send_pending_postback ) return;
 
+//http://parner.com/?var1={eventId}&date={date}&var3={datetime}&var4={dateUpdated}&var5={datetimeUpdated}&var5={name}&var6={opportunityId}&var7={currency}&var8={payout}&var9={userPayout}&var10={points}&var11={status}&var12={token}
+
+
+
+        /*        eventId = Stat tune event id
+date = Created (just date)
+datetime = Created
+dateUpdated = Updated (just date)
+datetimeUpdated = Updated
+name = Opportunity Name (matched via external ID)
+opportunityId = (opportunity ID matched via external ID)
+currency = Stat currency
+payout = Stat Payout
+userPayout = FIXED FOR NOW
+                       points = FIXED FOR NOW
+                                          status = depends or Partner settings
+    token = HARD CODED*/
+
         $url = $this->conversion->Partner->pending_url;
               doPostBackJob::dispatch(
                   $url
