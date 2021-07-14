ca61efe85dd2bc847aca65c8138341cd8db5ad3c
Илья <vayner.ilya@ya.ru>
2021-07-14 12:23:51 +0300

    to creation rule

--- app/Nova/Client.php
+++ app/Nova/Client.php
@@ -67,7 +67,8 @@ class Client extends Resource
             Select::make('Status')->options(self::$model::STATUSES)
                 ->rules('required')->sortable(),
             Text::make('Email')->sortable()
-                ->rules('required', 'email', 'max:255', 'unique:clients'),
+                ->rules('required', 'email', 'max:255')
+                ->creationRules('unique:clients'),
 
             DateTime::make('Created at')->sortable()->exceptOnForms(),
             DateTime::make('Updated at')->sortable()->exceptOnForms(),
