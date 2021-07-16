7ee0b05e74e67f464ba25c1ca9ef49b4a24abf39
770-2 desctop <iluxa@770studio.ru>
2021-07-16 10:54:21 +0300

    fix filter

--- app/Nova/Filters/DateTimeFilter.php
+++ app/Nova/Filters/DateTimeFilter.php
@@ -2,6 +2,7 @@
 
 namespace App\Nova\Filters;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Laravel\Nova\Filters\DateFilter;
@@ -11,16 +12,16 @@ class DateTimeFilter extends DateFilter
     /**
      * Apply the filter to the given query.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Database\Eloquent\Builder  $query
-     * @param  mixed  $value
-     * @return \Illuminate\Database\Eloquent\Builder
+     * @param Request $request
+     * @param Builder $query
+     * @param mixed $value
+     * @return Builder
      */
     public function apply(Request $request, $query, $value)
     {
 
 
-        return $query->whereDate('datetime',  Carbon::parse($value)->toDateString());
+        return $query->whereDate('Stat_datetime', Carbon::parse($value)->toDateString());
 
     }
 }
