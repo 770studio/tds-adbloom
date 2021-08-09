9007f3675809b1355c35cbd73f26ea90a9dbf4ec
Илья <vayner.ilya@ya.ru>
2021-08-09 11:12:54 +0300

    +m

--- database/migrations/2021_08_08_142100_add_points_name_to_partners.php
+++ database/migrations/2021_08_08_142100_add_points_name_to_partners.php
@@ -14,7 +14,7 @@ class AddPointsNameToPartners extends Migration
     public function up()
     {
         Schema::table('partners', function (Blueprint $table) {
-            $table->string('points_name');
+            $table->string('points_name')->nullable();
         });
     }
 
