37ad11ee4c75b055aedab6b4883bd98bef1cb0be
VI office <vayner.ilya@ya.ru>
2021-11-10 14:42:44 +0300

    on-page adjust, etc...

--- app/Helpers/WidgetJSTemplateHelper.php
+++ app/Helpers/WidgetJSTemplateHelper.php
@@ -10,41 +10,37 @@ class WidgetJSTemplateHelper
         $URL = config('app.url') . '/e.js';
 
         return <<<TPL
-
  <!-- adblm widget code -->
-    <script>
-      (function (w, d, t, r) {
-        var adblm = w.adblm = function () {
-          adblm.callMethod ? adblm.callMethod.apply(adblm, arguments) : adblm.queue.push(arguments)
-        }
-        adblm.push = adblm
-        adblm.queue = []
-        var n,
-          i
-        n = d.createElement(t)
-        n.src = r
-        n.async = 1
-        i = d.getElementsByTagName(t)[0]
-        i.parentNode.insertBefore(n, i)
-      })(window, document, 'script', '{$URL}/e.js')
+<script>
+  (function (w, d, t, r) {
+    var adblm = w.adblm = function () {
+      adblm.callMethod ? adblm.callMethod.apply(adblm, arguments) : adblm.queue.push(arguments)
+    }
+    adblm.push = adblm
+    adblm.queue = []
+    var n,
+      i
+    n = d.createElement(t)
+    n.src = r
+    n.async = 1
+    i = d.getElementsByTagName(t)[0]
+    i.parentNode.insertBefore(n, i)
+  })(window, document, 'script', '{$URL}')
 
-      const options = {
-        partnerId: '{$partnerId}',
-        widgetId: '{$widgetId}',
-        // userId: 'userId',
-        // clickId: 'clickId',
-        // email: 'email@email.com',
-        // country: 'UK',
-        // zip: '0000000',
-        // age: '20',
-        // birthdate: '1990-01-01',
-        // gender: 'male',
-        // clickSub: 'clickSub'
-      }
-
-      adblm('init', 'adblm-widget', options)
-    </script>
-
+  const options = {
+    partnerId: '{$partnerId}',
+    widgetId: '{$widgetId}',
+    // userId: 'userId',
+    // clickId: 'clickId',
+    // email: 'email@email.com',
+    // country: 'UK',
+    // zip: '0000000',
+    // age: '20',
+    // birthdate: '1990-01-01',
+    // gender: 'male',
+    // clickSub: 'clickSub'
+  }
+</script>
 TPL;
 
 
