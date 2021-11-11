83eae339c0ad800db768f5fb6275ba7cb03c3b4c
vayner.ilya <vayner.ilya@ya.ru>
2021-11-11 22:53:42 +0300

    comments to AF

--- app/Models/Infrastructure/ArrayField.php
+++ app/Models/Infrastructure/ArrayField.php
@@ -5,6 +5,7 @@ namespace App\Models\Infrastructure;
 
 
 use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Support\Collection;
 use ReflectionClass;
 
 abstract class ArrayField implements Arrayable
@@ -19,35 +20,57 @@ abstract class ArrayField implements Arrayable
 
     }
 
+    /**
+     *  if we have full house
+     *  e.g. all platforms : [1,2,3] == [1,2,3]
+     */
     public static function equalsToAll($array): bool
     {
         // dd(array_diff(static::all(), (array)$array));
         return !array_diff(static::all(), (array)$array);
     }
 
+    /**
+     *  key is a string key like "mobile" for platform
+     *  indexes are platform string keys: mobile, desktop, etc...
+     */
     public static function exists($key): bool
     {
         return in_array($key, static::indexes());
     }
 
+    /**
+     *  key is an id key like 1,2 or 3 for platform
+     *  all are platform id keys: 1,2 or 3 etc...
+     */
     public static function keyExists($key): bool
     {
         return in_array($key, static::all());
     }
 
-    public static function indexes()
+    /**
+     *  indexes are platform string keys: mobile, desktop, etc...
+     */
+    public static function indexes(): array
     {
         return array_keys(
             static::all()
         );
     }
 
-    public static function all()
+    /**
+     *  all array : 1=>mobile, 2=>desktop, etc...
+     */
+    public static function all(): array
     {
         $oClass = new ReflectionClass(static::class);
         return $oClass->getConstants();
     }
 
+    /**
+     *  if string key exists ('desktop') return its id
+     *
+     */
     public static function getName($key)
     {
         return static::exists($key)
@@ -55,26 +78,41 @@ abstract class ArrayField implements Arrayable
             : null;
     }
 
-    public static function collection()
+    /**
+     *  a collection formatted as:
+     *   1-> desktop,
+     *   2-> mobile,
+     *   etc..
+     */
+    public static function collection(): Collection
     {
         return collect(static::all_flipped());
     }
 
-    public static function all_flipped()
+    /**
+     *  an array formatted as:
+     *   1-> desktop,
+     *   2-> mobile,
+     *   etc..
+     */
+    public static function all_flipped(): array
     {
         return array_flip(
             static::all()
         );
     }
 
-    public static function values()
+    /**
+     *  return an array like [1,2,3] e.g. for platform
+     */
+    public static function values(): array
     {
         return array_values(
             static::all()
         );
     }
 
-    public function toArray()
+    public function toArray(): array
     {
         return static::all();
     }
