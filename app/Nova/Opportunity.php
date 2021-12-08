a85221a6cc3612af95bd57f6a97fba51171aa108
VI office <vayner.ilya@ya.ru>
2021-12-08 15:55:00 +0300

    remove url validation temporary

--- app/Nova/Opportunity.php
+++ app/Nova/Opportunity.php
@@ -120,7 +120,7 @@ class Opportunity extends Resource
                 })*/
 
             Text::make('Url', 'link')
-                ->rules('nullable', 'url')
+               // ->rules('nullable', 'url')
                 ->hideFromIndex(),
 
             Toggle::make('Use default macros', 'use_default_macros')->onColor('green'),
