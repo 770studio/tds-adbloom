34441dfebf0332a4decaf8a625703675fe07fbe3
VI office <vayner.ilya@ya.ru>
2021-12-08 15:20:40 +0300

    link url parse , parse reverse

---
+++ app/Helpers/UrlHelper.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Helpers;
+
+use LogicException;
+
+class UrlHelper
+{
+    public static function appendTo(string $url, string $append, $override = false): string
+    {
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            throw new LogicException('wrong url format');
+        }
+
+        $parts = parse_url($url);
+
+        parse_str($parts['query'], $query);
+        parse_str($append, $append_query);
+
+        $parts['query'] = $override
+            ? array_merge($query, $append_query)
+            : array_merge($append_query, $query);
+
+        return self::reverse_parse_url($parts);
+
+    }
+
+    public static function reverse_parse_url(array $parts)
+    {
+        $url = '';
+        if (!empty($parts['scheme'])) {
+            $url .= $parts['scheme'] . ':';
+        }
+        if (!empty($parts['user']) || !empty($parts['host'])) {
+            $url .= '//';
+        }
+        if (!empty($parts['user'])) {
+            $url .= $parts['user'];
+        }
+        if (!empty($parts['pass'])) {
+            $url .= ':' . $parts['pass'];
+        }
+        if (!empty($parts['user'])) {
+            $url .= '@';
+        }
+        if (!empty($parts['host'])) {
+            $url .= $parts['host'];
+        }
+        if (!empty($parts['port'])) {
+            $url .= ':' . $parts['port'];
+        }
+        if (!empty($parts['path'])) {
+            $url .= $parts['path'];
+        }
+        if (!empty($parts['query'])) {
+            if (is_array($parts['query'])) {
+                $url .= '?' . http_build_query($parts['query']);
+            } else {
+                $url .= '?' . $parts['query'];
+            }
+        }
+        if (!empty($parts['fragment'])) {
+            $url .= '#' . $parts['fragment'];
+        }
+
+        return $url;
+    }
+}
